ce1b899334fe349db7019beca2398856c1c74c59
kamuikatsurgi <shahkrishang11@gmail.com>
2025-12-01 14:44:52 +0530

    chore: nit

--- triedb/database.go
+++ triedb/database.go
@@ -380,8 +380,6 @@ func (db *Database) IsVerkle() bool {
 }
 
 func (db *Database) IsUsingTDB() bool {
-	log.Info("db.config.PathDB", "PathDB", db.config.PathDB)
-	log.Info("db.config.PathDB.UseTrieDB", "UseTrieDB", db.config.PathDB.UseTrieDB)
 	return db.config.PathDB != nil && db.config.PathDB.UseTrieDB
 }
 
